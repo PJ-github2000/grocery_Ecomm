7c409223c01478b23943af2ac9dd0f59cde48ac3
Praveen Gunja <gpraveen1542@gmail.com>
2024-12-28 14:51:11 +0530

    2.6

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -37,7 +37,7 @@ export const authOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/",
   },
